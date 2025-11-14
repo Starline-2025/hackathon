40b2e96fddf073e0a2b8e5a440303e139c46ce88
Andrey <snick25rs@gmail.com>
2025-11-15 00:46:40 +0100

    Реализует внедрение зависимости для сервиса работы с карточками.

--- backend/infra/di/di.py
+++ backend/infra/di/di.py
@@ -1,5 +1,9 @@
+from ...infra.repo import CardRepoImpl
 from ...app.usecase import CardNKOService
+from ...app.service import CardNKOServiceImpl
 
 
 def get_card_service() -> CardNKOService:
-    ...
+    repo = CardRepoImpl()
+    service = CardNKOServiceImpl(repo)
+    return service
