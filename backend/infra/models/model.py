d607c120621454ce38eb67f8d66823ee5ab92bf2
Andrey <snick25rs@gmail.com>
2025-11-14 19:03:59 +0100

    Определяет ORM-модель CardNKOORM для таблицы 'cards_nko'.

---
+++ backend/infra/models/model.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, PrimaryKeyConstraint, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class CardNKOORM(Base):
+    __tablename__ = 'cards_nko'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    contacts = Column(String, nullable=True)
+    city = Column(String, nullable=False)
+    img = Column(String, nullable=True)
+    website = Column(String, nullable=False)
+
