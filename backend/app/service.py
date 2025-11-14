c78d80358bc0601e5f6ea060a72a74c81af1553f
LeoHu <nleonteva2007@gmail.com>
2025-11-15 01:07:01 +0300

    добавила 0

--- backend/app/service.py
+++ backend/app/service.py
@@ -1,6 +1,6 @@
 """
 Надо реализовать class CardNKOService(файл usecase) назвав реализацию CardNKOServiceImpl
-
+000000
 Внутри этого класса надо вызывать функции CardRepo из domain/repo
 
 CardRepo должен передаваться в CardNKOServiceImpl в __init__
