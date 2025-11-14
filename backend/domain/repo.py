6514ab2eae1fc8b13b57d2ef4e69f652fcef4ba2
Andrey <snick25rs@gmail.com>
2025-11-14 19:11:52 +0100

    Реализует получение списка карточек с фильтрацией по параметрам.

--- backend/domain/repo.py
+++ backend/domain/repo.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
-
+from typing import List
 from ..domain.entity import Card
 
 
 class CardRepo(ABC):
 	@abstractmethod
-	def get_card(self, name : str | None, category: str | None, city: str | None) -> Card:
+	def get_cards(self, name : str | None, category: str | None, city: str | None) -> List[Card]:
 		pass
 	
 	@abstractmethod
--- backend/infra/repo.py
+++ backend/infra/repo.py
@@ -1,5 +1,4 @@
-from pip._internal.models import link
-
+from typing import List
 from ..domain.entity import Card
 from ..domain.repo import CardRepo
 from ..infra.db.db_connector import get_session
@@ -7,9 +6,20 @@ from ..infra.models.model import CardNKOORM
 
 
 class CardRepoImpl(CardRepo):
-	def get_card(self, name : str | None, category: str | None, city: str | None) -> Card:
+	def get_cards(self, name : str | None, category: str | None, city: str | None) -> List[Card]:
 		with get_session() as session:
-			...
+			query = session.query(CardNKOORM)
+			if name:
+				query = query.filter(CardNKOORM.name == name)
+			if category:
+				query = query.filter(CardNKOORM.category == category)
+			if city:
+				query = query.filter(CardNKOORM.city == city)
+			results = query.all()
+			if not results:
+				#TODO: Доделать ошибку
+				raise ...
+			return [Card.from_orm(obj) for obj in results]
 		
 	def create_card(self, card: Card) -> Card:
 		with get_session() as session:
