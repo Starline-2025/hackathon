963afa4ae2ecc7ed9d4fb3821dc6d5e86e6ceeb9
Andrey <snick25rs@gmail.com>
2025-11-14 18:50:54 +0100

    Заменяет адрес городом и добавляет метод from_orm для Entity.

--- backend/domain/entity.py
+++ backend/domain/entity.py
@@ -6,7 +6,18 @@ class Card:
 	category: str
 	description: str
 	contacts: str
-	address: str
+	city: str
 	img: str
 	website: str
 	
+	@classmethod
+	def from_orm(cls, card_orm):
+		return cls(
+			name=card_orm.name,
+			category=card_orm.category,
+			description=card_orm.description,
+			contacts=card_orm.contacts,
+			city=card_orm.city,
+			img=card_orm.img,
+			website=card_orm.website,
+		)
