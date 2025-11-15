ce5e65fc9b62366217e68054ecf090d7e55a80ed
Andrey <snick25rs@gmail.com>
2025-11-15 20:01:54 +0100

    Добавил импорт `Error` и исправил функцию `get_nko_service` в файле `di.py`.

--- backend/infra/di/di.py
+++ backend/infra/di/di.py
@@ -1,9 +1,13 @@
 from ...infra.repo import CardRepoImpl
 from ...app.usecase import CardNKOService
 from ...app.service import CardNKOServiceImpl
-
+from ...api.exceptions.error import Error
 
 def get_card_service() -> CardNKOService:
-    repo = CardRepoImpl()
-    service = CardNKOServiceImpl(repo)
-    return service
+	repo = CardRepoImpl()
+	service = CardNKOServiceImpl(repo)
+	return service
+
+def get_error() -> Error:
+	error = Error()
+	return error
