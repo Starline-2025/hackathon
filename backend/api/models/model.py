2af6f024a8760595a75573230c72b5b077608a92
Andrey <snick25rs@gmail.com>
2025-11-15 20:03:21 +0100

    Добавил модель данных `Card` с использованием Pydantic для обработки запросов.

--- backend/api/models/model.py
+++ backend/api/models/model.py
@@ -1,4 +1,17 @@
-"""
-Здесь модели Request и Response
-для POST запроса для добавления и GET для получения
-"""
+from pydantic import BaseModel, ConfigDict
+
+class Card(BaseModel):
+	model_config = ConfigDict(
+		populate_by_name=True,
+	)
+	name: str
+	category: str
+	description: str
+	city: str
+	website: str
+	lat: float
+	lng: float
+	address: str | None = None
+	contacts: str | None = None
+	img: str | None = None
+	
